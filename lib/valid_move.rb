a9daf2b3c3e6ed2ce553418f3318f492eacd8ec5
Allison MacQueen <allimacq@gmail.com>
2020-05-27 17:11:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   #first we need to check if the index is in the array of the board
-  if between?()
+  if index.between?(0,9)
     
 end
 
